09bf2c1d6aa50abf54d8ae036f96304ef4305cbf
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-04-26 18:11:28 +0200

    Fix adress

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb+srv://jurczas1:Mjmsod2011!@cluster0-sukdp.mongodb.net/test?retryWrites=tru', {
     useMongoClient: true
 });
 
